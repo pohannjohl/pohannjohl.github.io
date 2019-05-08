5c6b1cb49eeb493722a201f5844c2a4ae3ddd346
pohannjohl <pohlohann@t-online.de>
2019-05-08 14:29:43 +0200

    Update main.js

--- res/main.js
+++ res/main.js
@@ -1,1 +1,8 @@
-documend.getElementById("abc").innerHTML ="khgfdsayxcvbnm"
+cookies= 0;
+function click(){
+	cookies= cookies +1;
+	document.getElementById("cookiecounter").innerHTML= cookies;
+	
+	
+	
+}
